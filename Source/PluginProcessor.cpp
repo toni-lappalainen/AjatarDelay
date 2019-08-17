816640f55f68724667faaebd7f0628235f1c5268
CptNelson <toni.lappalainen@gmail.com>
2019-08-17 15:34:30 +0300

    working with better memory manager

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,9 +1,9 @@
 /*
   ==============================================================================
 
-    This file was auto-generated!
+	This file was auto-generated!
 
-    It contains the basic framework code for a JUCE plugin processor.
+	It contains the basic framework code for a JUCE plugin processor.
 
   ==============================================================================
 */
@@ -14,122 +14,116 @@
 //==============================================================================
 AjatarDelayAudioProcessor::AjatarDelayAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+	: AudioProcessor(BusesProperties()
+#if ! JucePlugin_IsMidiEffect
+#if ! JucePlugin_IsSynth
+		.withInput("Input", AudioChannelSet::stereo(), true)
+#endif
+		.withOutput("Output", AudioChannelSet::stereo(), true)
+#endif
+	),
+	treeState(*this, nullptr, "PARAMETER", createParameterLayout())
 #endif
 {
-//	mDelayBufferLeft = nullptr;
-	//mDelayBufferRight = nullptr;
 }
 
 AjatarDelayAudioProcessor::~AjatarDelayAudioProcessor()
 {
-	//ensure that buffers are nullptr when starting
-	/*
-	if (mDelayBufferLeft != nullptr)
-	{
-		delete[] mDelayBufferLeft;
-		mDelayBufferLeft = nullptr;
-	}
-	if (mDelayBufferRight != nullptr)
-	{
-		delete[] mDelayBufferRight;
-		mDelayBufferRight = nullptr;
-	}
-	*/
+}
+
+AudioProcessorValueTreeState::ParameterLayout AjatarDelayAudioProcessor::createParameterLayout()
+{
+	std::vector <std::unique_ptr<RangedAudioParameter>> params;
+	auto delayTimeParam = std::make_unique<AudioParameterFloat>(DELAYTIME_ID, DELAYTIME_NAME, 0.01f, 2.0f, 0.5f);
+	auto feedbackParam = std::make_unique<AudioParameterFloat>(FEEDBACK_ID, FEEDBACK_NAME, 0.0f, 0.98f, 0.6f);
+	auto smoothParam = std::make_unique<AudioParameterFloat>(SMOOTH_ID, SMOOTH_NAME, 0.001f, 0.02f, 0.006f);
+	auto dryWetParam = std::make_unique<AudioParameterFloat>(DRYWET_ID, DRYWET_NAME, 0.0f, 1.0f, 0.5f);
+
+	params.push_back(std::move(delayTimeParam));
+	params.push_back(std::move(feedbackParam));
+	params.push_back(std::move(smoothParam));
+	params.push_back(std::move(dryWetParam));
+	return { params.begin(), params.end() };
 }
 
 
 //==============================================================================
-void AjatarDelayAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void AjatarDelayAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
 	mDelayBufferLength = 2.0 * (samplesPerBlock + sampleRate);
-	
-	mDelayTimeSmoothed = 0.3f;
+
+	mDelayTimeSmoothed = *treeState.getRawParameterValue(DELAYTIME_ID);
 	mDelayTimeInSamples = sampleRate * mDelayTimeSmoothed;
+
 	//set delay buffers as float arrays and fill the memories with zeroes
-		/*
-	if (mDelayBufferLeft == nullptr)
-	{
-		mDelayBufferLeft = new float[mDelayBufferLength];
-	}
-	zeromem(mDelayBufferLeft, mDelayBufferLength * sizeof(float));
+	mDelayBufferLeft = std::make_unique<float[]>(mDelayBufferLength);
+	mDelayBufferRight = std::make_unique<float[]>(mDelayBufferLength);
 
-	if (mDelayBufferRight == nullptr)
-	{
-		mDelayBufferRight = new float[mDelayBufferLength];
-	}
-	zeromem(mDelayBufferRight, mDelayBufferLength * sizeof(float));
-	*/
+	zeromem(mDelayBufferLeft.get(), mDelayBufferLength * sizeof(float));
+	zeromem(mDelayBufferRight.get(), mDelayBufferLength * sizeof(float));
 
-	mDelayBufferLeft.reset(new float[mDelayBufferLength]);
-	mDelayBufferRight.reset(new float[mDelayBufferLength]);
 }
 
 void AjatarDelayAudioProcessor::releaseResources()
 {
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
+	// When playback stops, you can use this as an opportunity to free up any
+	// spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool AjatarDelayAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool AjatarDelayAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
-        return false;
+#if JucePlugin_IsMidiEffect
+	ignoreUnused(layouts);
+	return true;
+#else
+	// This is the place where you check if the layout is supported.
+	// In this template code we only support mono or stereo.
+	if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+		&& layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+		return false;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
+	// This checks if the input layout matches the output layout
+#if ! JucePlugin_IsSynth
+	if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+		return false;
+#endif
 
-    return true;
-  #endif
+	return true;
+#endif
 }
 #endif
 
-void AjatarDelayAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
+void AjatarDelayAudioProcessor::processBlock(AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
 {
-    ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
+	ScopedNoDenormals noDenormals;
+	auto totalNumInputChannels = getTotalNumInputChannels();
+	auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+	for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+		buffer.clear(i, 0, buffer.getNumSamples());
 
 	//get the float pointers to both channels of buffer
-	float* mainBufferLeftChannel = buffer.getWritePointer(0);
-	float* mainBufferRightChannel = buffer.getWritePointer(1);
+	const float* mainBufferLeftChannel = buffer.getReadPointer(0);
+	const float* mainBufferRightChannel = buffer.getReadPointer(1);
 
-	float smoothValue = 0.006f;
-	float delayTimeValue = 0.5f;
-	float feedbackValue = 0.9f;
 
-    for (int sample = 0; sample < buffer.getNumSamples(); sample++)
-    {
+	for (int sample = 0; sample < buffer.getNumSamples(); sample++)
+	{
+		float delayTimeValue = *treeState.getRawParameterValue(DELAYTIME_ID);
+		float feedbackValue = *treeState.getRawParameterValue(FEEDBACK_ID);
+		float smoothValue = *treeState.getRawParameterValue(SMOOTH_ID);
+		float dryWetValue = *treeState.getRawParameterValue(DRYWET_ID);
+
 		// calculate the smoothed delaytimes. 
 		mDelayTimeSmoothed = mDelayTimeSmoothed - smoothValue * (mDelayTimeSmoothed - delayTimeValue);
-		
+
 		// get the delay time in samples
 		mDelayTimeInSamples = getSampleRate() * mDelayTimeSmoothed;
 
 		// set the delaybuffers at delayWritePosition to mainBuffers at [sample] location
-		mDelayBufferLeft[mDelayBufferWritePosition] = mainBufferLeftChannel[sample];
-		mDelayBufferRight[mDelayBufferWritePosition] = mainBufferRightChannel[sample];
+		mDelayBufferLeft[mDelayBufferWritePosition] = mainBufferLeftChannel[sample] + mFeedBackLeft;
+		mDelayBufferRight[mDelayBufferWritePosition] = mainBufferRightChannel[sample] + mFeedBackRight;
 
 		// set delay read position to delay write position - delay time in samples
 		// and check if delay read position is smaller than zero
@@ -151,8 +145,8 @@ void AjatarDelayAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBu
 		if (readPosition_x1 >= mDelayBufferLength)
 			readPosition_x1 -= mDelayBufferLength;
 
-		float lerpedSampleLeft = mDelayBufferLeft[readPosition_x0];//lerp(mDelayBufferLeft[readPosition_x0], mDelayBufferLeft[readPosition_x1], readPositionFloat);
-		float lerpedSampleRight = mDelayBufferRight[readPosition_x0];//lerp(mDelayBufferRight[readPosition_x0], mDelayBufferRight[readPosition_x1], readPositionFloat);
+		float lerpedSampleLeft = lerp(mDelayBufferLeft[readPosition_x0], mDelayBufferLeft[readPosition_x1], readPositionFloat);
+		float lerpedSampleRight = lerp(mDelayBufferRight[readPosition_x0], mDelayBufferRight[readPosition_x1], readPositionFloat);
 
 		// set the feedbacks to interpolated float * feedbackValue
 		mFeedBackLeft = lerpedSampleLeft * feedbackValue;
@@ -160,12 +154,12 @@ void AjatarDelayAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBu
 		// advance the delay write position
 		mDelayBufferWritePosition++;
 		//set the buffer samples to current sample * (1 - *drywetValue) + lerp * *dryWetValue
-		buffer.setSample(0, sample, buffer.getSample(0, sample) * (1 - 0.5f) + lerpedSampleLeft * 0.5f);
-		buffer.setSample(1, sample, buffer.getSample(1, sample) * (1 - 0.5f) + lerpedSampleRight * 0.5f);
+		buffer.setSample(0, sample, buffer.getSample(0, sample) * (1 - dryWetValue) + lerpedSampleLeft * dryWetValue);
+		buffer.setSample(1, sample, buffer.getSample(1, sample) * (1 - dryWetValue) + lerpedSampleRight * dryWetValue);
 		//Check if the delay buffer position is >= than the lenght of the delay buffer, if so, set it to zero so it wraps to beginning.
 		if (mDelayBufferWritePosition >= mDelayBufferLength)
 			mDelayBufferWritePosition = 0;
-    }
+	}
 }
 
 float AjatarDelayAudioProcessor::lerp(float x0, float x1, float phase) {
@@ -175,94 +169,94 @@ float AjatarDelayAudioProcessor::lerp(float x0, float x1, float phase) {
 //==============================================================================
 bool AjatarDelayAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+	return true; // (change this to false if you choose to not supply an editor)
 }
 
 AudioProcessorEditor* AjatarDelayAudioProcessor::createEditor()
 {
-    return new AjatarDelayAudioProcessorEditor (*this);
+	return new AjatarDelayAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void AjatarDelayAudioProcessor::getStateInformation (MemoryBlock& destData)
+void AjatarDelayAudioProcessor::getStateInformation(MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+	// You should use this method to store your parameters in the memory block.
+	// You could do that either as raw data, or use the XML or ValueTree classes
+	// as intermediaries to make it easy to save and load complex data.
 }
 
-void AjatarDelayAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void AjatarDelayAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+	// You should use this method to restore your parameters from this memory block,
+	// whose contents will have been created by the getStateInformation() call.
 }
 
 //==============================================================================
 // This creates new instances of the plugin..
 AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
-    return new AjatarDelayAudioProcessor();
+	return new AjatarDelayAudioProcessor();
 }
 
 //==============================================================================
 const String AjatarDelayAudioProcessor::getName() const
 {
-    return JucePlugin_Name;
+	return JucePlugin_Name;
 }
 
 bool AjatarDelayAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_WantsMidiInput
+	return true;
+#else
+	return false;
+#endif
 }
 
 bool AjatarDelayAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_ProducesMidiOutput
+	return true;
+#else
+	return false;
+#endif
 }
 
 bool AjatarDelayAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_IsMidiEffect
+	return true;
+#else
+	return false;
+#endif
 }
 
 double AjatarDelayAudioProcessor::getTailLengthSeconds() const
 {
-    return 0.0;
+	return 0.0;
 }
 
 int AjatarDelayAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+	return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+				// so this should be at least 1, even if you're not really implementing programs.
 }
 
 int AjatarDelayAudioProcessor::getCurrentProgram()
 {
-    return 0;
+	return 0;
 }
 
-void AjatarDelayAudioProcessor::setCurrentProgram (int index)
+void AjatarDelayAudioProcessor::setCurrentProgram(int index)
 {
 }
 
-const String AjatarDelayAudioProcessor::getProgramName (int index)
+const String AjatarDelayAudioProcessor::getProgramName(int index)
 {
-    return {};
+	return {};
 }
 
-void AjatarDelayAudioProcessor::changeProgramName (int index, const String& newName)
+void AjatarDelayAudioProcessor::changeProgramName(int index, const String& newName)
 {
 }
 
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -12,6 +12,15 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+#define DELAYTIME_ID "delaytime"
+#define DELAYTIME_NAME "DelayTime"
+#define FEEDBACK_ID "feedback"
+#define FEEDBACK_NAME "Feedback"
+#define DRYWET_ID "drywet"
+#define DRYWET_NAME "DryWet"
+#define SMOOTH_ID "smooth"
+#define SMOOTH_NAME "Smooth"
+
 //==============================================================================
 /**
 */
@@ -56,10 +65,15 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 	float lerp(float v0, float v1, float t);
 
+	AudioProcessorValueTreeState treeState;
+	AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
 private:
 
 	std::unique_ptr<float[]> mDelayBufferLeft;
 	std::unique_ptr<float[]> mDelayBufferRight;
+//	float* mDelayBufferLeft;
+//	float* mDelayBufferRight;
 
 	float mDelayReadPosition{ 0.0f };
 	float mDelayTimeInSamples{ 0.0f };
